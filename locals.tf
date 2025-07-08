bd3e09c24e4258037285942283c49199b5b7b94f
varshini <varshini.reddy1@gmail.com>
2025-07-08 22:28:48 +1000

    module edited

--- locals.tf
+++ locals.tf
@@ -4,12 +4,12 @@ locals {
     private_subnet_id = split ("," , data.aws_ssm_parameter.private_subnet_ids.value)[0]
     private_subnet_ids = split ("," , data.aws_ssm_parameter.private_subnet_ids.value)
 
-    ami_id = data.aws_ami.joindevops.id
-    sg_id = data.aws_ssm_parameter.sg_id.value
-
     backend_alb_listener_arn = data.aws_ssm_parameter.backend_alb_listener_arn.value
     frontend_alb_listener_arn = data.aws_ssm_parameter.frontend_alb_listener_arn.value
 
+    ami_id = data.aws_ami.joindevops.id
+    sg_id = data.aws_ssm_parameter.sg_id.value
+   
     alb_listener_arn = "${var.component}" == "frontend" ? local.frontend_alb_listener_arn : local.backend_alb_listener_arn
 
     tg_port = "${var.component}" == "frontend" ? 80 : 8080
--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ resource "aws_lb_target_group" "main" {
     healthy_threshold = 2
     interval = 5
     matcher = "200-299"
-    path = "/health"
+    path = local.health_check_path
     port = local.tg_port
     timeout = 2
     unhealthy_threshold = 3
